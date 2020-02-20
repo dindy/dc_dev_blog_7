1508759b97a104c788f54c15c80291149eee49f4
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-20 11:30:13 +0100

    Ajout de fixtures pour les posts

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -15,9 +15,23 @@ class PostFixtures extends Fixture
         $post->setTitle('Title 1');
         $post->setContent('Content 1');
         $post->setCreated(new \DateTime('2011-01-01T15:03:01'));
-        
-        // Enregistrement en bdd
         $manager->persist($post);
+        
+        // Définition d'un nouveau post
+        $post = new Post();
+        $post->setTitle('Title 2');
+        $post->setContent('Content 2');
+        $post->setCreated(new \DateTime('2011-01-01T15:03:01'));        
+        $manager->persist($post);
+        
+        // Définition d'un nouveau post
+        $post = new Post();
+        $post->setTitle('Title 3');
+        $post->setContent('Content 3');
+        $post->setCreated(new \DateTime('2011-01-01T15:03:01'));
+        $manager->persist($post);
+
+        // Enregistrement en bdd
         $manager->flush();
     }
 }
