a66cd1ddf114c6044dd984656475a45221182f99
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-20 15:38:25 +0100

    #WIP Ajout du menu par catégorie

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,18 +15,31 @@ class BlogController extends AbstractController
         $postsRepository = $doctrine->getRepository(Post::class);
         $posts = $postsRepository->findAll();
         
-        return $this->render('home.html.twig', ['posts' => $posts]);
-    }
-    
-    public function post($id) {
-        $doctrine = $this->getDoctrine();
-        $postsRepository = $doctrine->getRepository(Post::class);
-        $post = $postsRepository->find($id);
-
-        if (!$post) {
-            throw $this->createNotFoundException('Pas d\'article trouvé pour l\'id '.$id);            
+        $categoriesRepository = $doctrine->getRepository(Category::class);
+        $categories = $categoriesRepository->findAll();
+        
+        return $this->render('home.html.twig', [
+            'posts' => $posts,
+            'categories' => $categories
+            ]);
         }
-
-        return $this->render('post.html.twig', ['post' => $post]);
+        
+        public function post($id) 
+        {
+            $doctrine = $this->getDoctrine();
+            $postsRepository = $doctrine->getRepository(Post::class);
+            $post = $postsRepository->find($id);
+            
+            if (!$post) {
+                throw $this->createNotFoundException('Pas d\'article trouvé pour l\'id '.$id);            
+            }
+            
+            $categoriesRepository = $doctrine->getRepository(Category::class);
+            $categories = $categoriesRepository->findAll();
+            
+            return $this->render('post.html.twig', [
+                'post' => $post,
+                'categories' => $categories
+            ]);
     }     
 }
