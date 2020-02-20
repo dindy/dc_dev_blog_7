6e4a86c68af18598ce9a1eef7c50e292960a2ef1
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-20 14:47:21 +0100

    Ajout de fixtures pour les catégories

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -12,20 +12,21 @@ class PostFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // Définition d'une nouvelle catégorie 
-        $category = new Category();
-        $category->setName('Catégorie 1');
-        $manager->persist($category);
+        $category1 = new Category();
+        $category1->setName('Catégorie 1');
+        $manager->persist($category1);
 
         // Définition d'une nouvelle catégorie 
-        $category = new Category();
-        $category->setName('Catégorie 2');
-        $manager->persist($category);
+        $category2 = new Category();
+        $category2->setName('Catégorie 2');
+        $manager->persist($category2);
 
         // Définition d'un nouveau post
         $post = new Post();
         $post->setTitle('Title 1');
         $post->setContent('Content 1');
         $post->setCreated(new \DateTime('2011-01-01T15:03:01'));
+        $post->setCategory($category1);
         $manager->persist($post);
         
         // Définition d'un nouveau post
@@ -33,6 +34,7 @@ class PostFixtures extends Fixture
         $post->setTitle('Title 2');
         $post->setContent('Content 2');
         $post->setCreated(new \DateTime('2011-01-01T15:03:01'));        
+        $post->setCategory($category2);
         $manager->persist($post);
         
         // Définition d'un nouveau post
@@ -40,6 +42,7 @@ class PostFixtures extends Fixture
         $post->setTitle('Title 3');
         $post->setContent('Content 3');
         $post->setCreated(new \DateTime('2011-01-01T15:03:01'));
+        $post->setCategory($category1);
         $manager->persist($post);
 
         // Enregistrement en bdd
