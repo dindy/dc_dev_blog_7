ad1ca1ea1dbe2e0c2b384b9b0542b02c779db5f8
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-20 11:47:20 +0100

    Affiche la liste des articles sur la page d'accueil

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,6 +10,10 @@ class BlogController extends AbstractController
 {
     public function list()
     {
-        return new Response('<html><head></head><body>Hello world</body></html>');
+        $doctrine = $this->getDoctrine();
+        $postsRepository = $doctrine->getRepository(Post::class);
+        $posts = $postsRepository->findAll();
+
+        return $this->render('home.html.twig', ['posts' => $posts]);
     }
 } 
